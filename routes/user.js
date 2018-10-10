61f1bbbc6853e8ec7a193c85637d25b976fce7ec
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-10 23:28:13 +0200

    adding get y put to user/task and user/items

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/User')
+
 router.post("/", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
@@ -12,4 +14,36 @@ router.put("/:id", (req, res, next) => {
 router.delete("/:id", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
+
+router.get('/:id/tasks',(req,res,next)=>{
+  const {id}=req.params
+  User.findById(id,'tasks')
+  .then(task=>{
+   return res.json(task)
+  })
+  .catch(e=>next(e))
+})
+router.put('/:id/tasks/:taskId',(req,res,next)=>{
+  const {id,taskId}=req.params
+  console.log("bbb",req.params)
+  User.findByIdAndUpdate(id,{$push:{tasks:id}},{new:true})
+  .then(user=>res.json({id,user}))
+  .catch(e=>next(e))
+})
+
+router.get('/:id/items',(req,res,next)=>{
+  const {id}=req.params
+  User.findById(id,'items')
+  .then(item=>res.json(item))
+  .catch(e=>next(e))
+})
+router.put('/:id/items/:itemId',(req,res,next)=>{
+  const {id,itemId}=req.params
+  console.log(req.params)
+  User.findByIdAndUpdate(id,{$push:{items:id}},{new:true})
+  .then(user=>res.json({id,user}))
+  .catch(e=>next(e))
+})
+
+
 module.exports = router
