e6f4f6977fada90e3322e8dea579fec7deee9206
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-10 23:27:07 +0200

    fix genericCRUD

--- routes/genericCRUD.js
+++ routes/genericCRUD.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const passport = require("passport");
-const router = express.Router();
 genericCRUD = model => {
+  const express = require("express");
+  const router = express.Router();
   const Model = require(`../models/${model}`);
 
   router.get("/", (req, res, next) => {
