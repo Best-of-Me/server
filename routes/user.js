b9940627ba8c7c6f75536ee2fbf7bb0c4a18062d
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-17 15:04:09 +0200

    Change variable name

--- routes/user.js
+++ routes/user.js
@@ -7,19 +7,19 @@ const ObjectId = require("mongoose").Types.ObjectId;
 router.post("/", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
-router.post("/:id", (req, res, next) => {
+router.post("/:userId", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
-router.put("/:id", (req, res, next) => {
+router.put("/:userId", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
-router.delete("/:id", (req, res, next) => {
+router.delete("/:userId", (req, res, next) => {
   res.status(404).end('/user not-found')
 });
 
-router.get('/:id/tasks',(req,res,next)=>{
-  const {id}=req.params
-  User.findById(id,'tasks')
+router.get('/:userId/tasks',(req,res,next)=>{
+  const {userId}=req.params
+  User.findById(userId,'tasks')
   .then(task=>{
    return res.json(task)
   })
@@ -65,17 +65,16 @@ router.put('/:userId/tasks/:taskId',(req,res,next)=>{
   .catch(e=>next(e))
 })
 
-router.get('/:id/items',(req,res,next)=>{
-  const {id}=req.params
-  User.findById(id,'items')
+router.get('/:userId/items',(req,res,next)=>{
+  const {userId}=req.params
+  User.findById(userId,'items')
   .then(item=>res.json(item))
   .catch(e=>next(e))
 })
-router.put('/:id/items/:itemId',(req,res,next)=>{
-  const {id,itemId}=req.params
-  console.log(req.params)
-  User.findByIdAndUpdate(id,{$push:{items:id}},{new:true})
-  .then(user=>res.json({id,user}))
+router.put('/:userId/items/:itemId',(req,res,next)=>{
+  const {userId,itemId}=req.params
+  User.findByIdAndUpdate(userId,{$push:{items:itemId}},{new:true})
+  .then(user=>res.json(user))
   .catch(e=>next(e))
 })
 
